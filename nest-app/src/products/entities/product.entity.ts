aeb20b355d7514edd02d1b48519c6acc1de5fe00
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-12-06 21:35:25 +0200

    feat: entity: now has a primary column

--- nest-app/src/products/entities/product.entity.ts
+++ nest-app/src/products/entities/product.entity.ts
@@ -1,7 +1,13 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Product {
   @Field(() => Int, { description: 'Example field (placeholder)' })
   exampleField: number;
+
+  @PrimaryGeneratedColumn()
+  @Field(() => Int)
+  id: number;
 }

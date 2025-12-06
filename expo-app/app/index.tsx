bc9c50e35041a43ff8534f0f9cd484ee85ba2b94
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-12-07 00:09:01 +0200

    feat: ProductCard: added a delete button

--- expo-app/app/index.tsx
+++ expo-app/app/index.tsx
@@ -1,8 +1,7 @@
-import { Text, View } from "react-native";
-import { apolloClient } from "./_layout";
 import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client/react";
-import { FlatList } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 const GET_PRODUCTS = gql`
   query GetProducts {
@@ -16,7 +15,7 @@ const GET_PRODUCTS = gql`
 `;
 
 export default function Index() {
-  const { loading, error, data } = useQuery(GET_PRODUCTS);
+  const { loading, error, data, refetch } = useQuery(GET_PRODUCTS);
 
   if (loading) return <Text>Loading...</Text>;
   if (error) return <Text>Error: {error.message}</Text>;
@@ -33,6 +32,9 @@ export default function Index() {
         data={data.products}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => <ProductCard product={item} />}
+        onRefresh={refetch}
+        refreshing={loading}
+        ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
       />
     </View>
   );
@@ -49,19 +51,41 @@ const ProductCard = ({ product }: { product: ProductType }) => {
   return (
     <View
       style={{
+        alignSelf: "stretch",
+        flexDirection: "row",
         borderWidth: 1,
         borderColor: "#ccc",
         borderRadius: 8,
         padding: 16,
-        marginBottom: 16,
-        //width: "80%",
       }}
     >
-      <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 8 }}>
-        {product.name}
-      </Text>
-      <Text style={{ marginBottom: 8 }}>{product.description}</Text>
-      <Text style={{ fontSize: 16, fontWeight: "600" }}>${product.price}</Text>
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 8 }}>
+          {product.name}
+        </Text>
+        <Text style={{ marginBottom: 8 }}>{product.description}</Text>
+        <Text style={{ fontSize: 16, fontWeight: "600" }}>
+          ${product.price}
+        </Text>
+      </View>
+      <TouchableOpacity
+        style={{
+          //justifyContent: "center",
+          //alignItems: "center",
+          padding: 8,
+          //marginLeft: 16,
+          alignSelf: "flex-start",
+          borderRadius: 4,
+          borderColor: "#000000",
+          borderWidth: 1,
+        }}
+      >
+        <FontAwesome6 name="trash" size={20} color="red" />
+      </TouchableOpacity>
     </View>
   );
 };
